bca3c455ce2eb2fbfb1db6e90158a033ab3c9df1
Álex González <agonzalezro@gmail.com>
2013-07-06 22:04:29 +0100

    Finally the Keys are being returned properly for each user

--- pepita.js
+++ pepita.js
@@ -2,55 +2,60 @@ Users = new Meteor.Collection("users");
 Pages = new Meteor.Collection("pages");
 Keys = new Meteor.Collection("keys");
 
-
-var createTestData = function() {
-  var defaultPageId = Pages.insert({
-    "name" : "Pepita's homepage"
-  });
-  Users.remove({}, function () {
-    Users.insert({
-      "username" : "pepita",
-      "history" : [],
-      "defaultPageId" : defaultPageId
+/*
+ * CLIENT code
+ */
+if (Meteor.isClient) {
+  Template.keyboard.keys = function () {
+    var user = Users.findOne({
+      username: "pepita"
     });
-    Keys.remove({} , function () {
-      Keys.insert({
-        "pageId" : defaultPageId,
-        "keyNumber" : 4,
-        "command" : "tel://00447449601002",
-        "image" : "images/alexKey.jpg"
+    if (user) {
+      var pageId = _.last([user.defaultPageId].concat(user.history));
+      return Keys.find({
+        pageId: pageId
       });
-      Keys.insert({
-        "pageId" : defaultPageId,
-        "keyNumber" : 5, "command" :
-        "Call Carlos",
-        "image" : "images/carlosKey.jpg"
-      });
-    });
-  });
-  return defaultPageId;
+    }
+  }
 }
 
 
-var startupApplicationServer =  function () {
-  // This creates some sample data
-  Pages.remove({}, createTestData);
-  console.log("startupApplicationServer");
+/*
+ * SERVER code
+ */
+var startupApplicationServer = function() {
+  if (Users.find().count() === 0) {
+    var defaultPageId = Pages.insert({
+      "name" : "Pepita's homepage"
+    });
+
+    Users.remove({}, function () {
+      Users.insert({
+        username: "pepita",
+        history: [],
+        defaultPageId: defaultPageId
+      });
+      Keys.remove({} , function () {
+        Keys.insert({
+          pageId: defaultPageId,
+          keyNumber: 4,
+          command: "tel://00447449601002",
+          image: "images/alexKey.jpg"
+        });
+        Keys.insert({
+          pageId: defaultPageId,
+          keyNumber: 5,
+          command: "Call Carlos",
+          image: "images/carlosKey.jpg"
+        });
+      });
+    });
+
+    console.log("Mock data created!");
+  }
 }
 
 
 if (Meteor.isServer) {
   Meteor.startup(startupApplicationServer);
-} else if (Meteor.isClient) {
-  var user = Users.findOne({
-    "username": "pepita"
-  });
-  console.log(user);
-  Template.keyboard.keys = function () {
-    var pageId = _.last([user.defaultPageId].concat(user.history));
-    return Keys.find({
-      pageId: pageId
-    });
-  };
-
 }
