991d1e5b9ee25e6e33fd30b7f4647fb9a8d945e5
Álex González <agonzalezro@gmail.com>
2013-07-07 02:21:32 +0100

    There is a crapy navigation at the moment
    What I think that I should have done:
    
        - create views
        - bind events
        - use the event to load the new view
    
    What I have done:
    
        - create links with javascript:// href
        - this functions will remove all the Keys and...
        - they will generate the new needed Keys

--- pepita.js
+++ pepita.js
@@ -12,50 +12,127 @@ if (Meteor.isClient) {
     });
     if (user) {
       var pageId = _.last([user.defaultPageId].concat(user.history));
+      var keys = [];
+      for (i=0;i<=10;i++) {
+
+      }
       return Keys.find({
         pageId: pageId
+      }, {
+        sort: {keyNumber: 1}
       });
     }
   }
+
+  /* Navigation functions
+   * The way of saving it doesn't seem so nice, but ehy! It's a hackathon :D
+   */
+  Template.pepitaEventsWeather = function () {
+    var user = Users.findOne({
+      username: "pepita"
+    });
+
+    Meteor.call("removeKeys", function() {
+      Keys.insert({
+        pageId: user.defaultPageId,
+        keyNumber: 1,
+        text: "24° now"
+      }),
+      Keys.insert({
+        pageId: user.defaultPageId,
+        keyNumber: 2,
+        command: "javascript:Template.pepitaEventsIndex();",
+        text: "Go back"
+      })
+    });
+  }
+
+  Template.pepitaEventsIndex = function() {
+    var user = Users.findOne({
+      username: "pepita"
+    });
+
+    Meteor.call("removeKeys", function () {
+      Meteor.call("generateIndex", user.defaultPageId)
+    });
+  };
 }
 
 
 /*
  * SERVER code
  */
-var startupApplicationServer = function() {
-  if (Users.find().count() === 0) {
+var createMockupData = function (defaultPageId) {
+  if (!defaultPageId) {
     var defaultPageId = Pages.insert({
       "name" : "Pepita's homepage"
     });
 
-    Users.remove({}, function () {
-      Users.insert({
-        username: "pepita",
-        history: [],
-        defaultPageId: defaultPageId
-      });
-      Keys.remove({} , function () {
+    Users.insert({
+      username: "pepita",
+      history: [],
+      defaultPageId: defaultPageId
+    });
+  }
+
+  // Create the 10 possible keys (just for testing purposes)
+  for (var index=0; index<=10; index++) {
+    switch(index) {
+      case 10:
         Keys.insert({
           pageId: defaultPageId,
-          keyNumber: 4,
-          command: "tel://00447449601002",
-          image: "images/alexKey.jpg"
-        });
-        Keys.insert({
-          pageId: defaultPageId,
-          keyNumber: 5,
+          keyNumber: index,
           command: "Call Carlos",
           image: "images/carlosKey.jpg"
-        });
+        })
+        break;
+      case 9:
+        Keys.insert({
+          pageId: defaultPageId,
+          keyNumber: index,
+          command: "tel://00447449601002",
+          image: "images/alexKey.jpg"
+        })
+        break;
+      case 8:
+        Keys.insert({
+          pageId: defaultPageId,
+          keyNumber: index,
+          command: "javascript:Template.pepitaEventsWeather();",
+          image: "images/weather.png"
+        })
+      /*default:
+        Keys.insert({
+          pageId: defaultPageId,
+          keyNumber: index,
+          image: images
+        })
+        break;*/
+    }
+  }
+}
+
+
+var startupApplicationServer = function() {
+  if (Users.find().count() === 0 || true) {  // Force it for now
+    // Delete all
+    Users.remove({}, function() {
+      Pages.remove({}, function() {
+        Keys.remove({}, createMockupData);
       });
     });
-
-    console.log("Mock data created!");
   }
 }
 
 
 if (Meteor.isServer) {
   Meteor.startup(startupApplicationServer);
+  Meteor.methods({
+    removeKeys: function () {
+      Keys.remove({});
+    },
+    generateIndex: function (defaultPageId) {
+      createMockupData(defaultPageId);
+    }
+  });
 }
